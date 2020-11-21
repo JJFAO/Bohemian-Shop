8163b0c849b75a76a3d496082d6fec6138dbfbbe
Mauro Bringas <maurosbringas@gmail.com>
2020-11-20 23:08:03 -0300

    Funcionalidad eliminar e editar productos

--- js/admin.js
+++ js/admin.js
@@ -3,6 +3,10 @@ const newPrice = document.getElementById('addProductPrice')
 const addImageBtn = document.getElementById('addImageBtn')
 const newProductForm = document.getElementById('newProductForm')
 const productTable = document.getElementById('productTable')
+const editedName = document.getElementById('editedName')
+const editedPrice = document.getElementById('editedPrice')
+const editedImage = document.getElementById('editedImage')
+const editProductForm = document.getElementById('editProductForm')
 
 const generateId = function () {
     return '_' + Math.random().toString(36).substr(2, 9);
@@ -25,7 +29,6 @@ newProductForm.onsubmit = (e) => {
     })
 
     localStorage.setItem('products', JSON.stringify(products))
-    console.log(products);
     newProductForm.reset();
     displayAllProducts()
 }
@@ -39,13 +42,24 @@ function displayProducts(products) {
         <tr>
             <th scope="row">${product.name}</th>
                 <td>
-                <img src="${product.image}"alt="nada" class="product-image">
+                <div class="text-center"">
+                <img src="${product.image}"alt="Sin imagen" class="product-image">
+                </div>
                 </td>
                 <td>${product.price}</td>
                 <td>${product.id}</td>
+                <td>
+                <div class="text-center">
+                <div class="p-2">
+                <button class="btn btn-outline-danger" onclick="deleteProduct('${product.id}')">Eliminar</button>
+                </div>
+                <div class="p-2">
+                <button class="btn btn-outline-info" data-toggle="modal" data-target="#editProductModal" onclick="loadForm('${product.id}')">Editar</button>
+                </div>
+                </div>
+                </td>
         </tr>
         `
-        console.log(product.image);
         almacenator.push(tbodyContent)
     }
 
@@ -59,4 +73,49 @@ function displayAllProducts() {
 }
 
 displayAllProducts()
+
+function deleteProduct(productId) {
+    const products = JSON.parse(localStorage.getItem('products')) || [];
+    const filteredProducts = products.filter((products) => products.id !== productId);
+    localStorage.setItem('products', JSON.stringify(filteredProducts));
+    displayAllProducts();
+}
+
+const loadForm = (productId) => {
+    const products = JSON.parse(localStorage.getItem('products')) || [];
+    const product = products.find(p => p.id === productId);
+    editedName.value = product.name;
+    editedPrice.value = product.price;
+    editedImage.value = product.image;
+    editProductId = productId;
+}
+
+editProductForm.onsubmit = (e) => {
+    e.preventDefault()
+    const products = JSON.parse(localStorage.getItem('products')) || [];
+    const name = editedName.value;
+    const price = editedPrice.value;
+    const image = editedImage.value;
+    const updatedAt = Date.now();
+    
+    const updateProducts = products.map((p) => {
+      if (p.id === editProductId) {
+        const products = {
+          ...p,
+          name,
+          price,
+          image,
+          updatedAt,
+        }
+        return products;
+      } else {
+        return p;
+      }
+    });
+  
+    localStorage.setItem('products', JSON.stringify(updateProducts));
+    editProductForm.reset();
+    displayAllProducts();
+    $('#editProductModal').modal('hide')
+  }
 
