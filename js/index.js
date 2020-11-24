530b7bb082b1bc309473c22e33a5082809fbe987
Poly <66856601+Paulazarte@users.noreply.github.com>
2020-11-24 10:10:00 -0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,10 @@
+/*NAVABAR EFFECT */
+$(function () {
+    $(document).scroll(function () {
+      var $nav = $(".sticky-top");
+      $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+    });
+  });
+$(".img-fluid").addClass("wow fadeIn z-depth-1-half");
+
+
