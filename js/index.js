aba0ee34d53dba0d1a4f8a8abcd7b2f3e89bfa5d
Mauro Bringas <maurosbringas@gmail.com>
2020-11-30 14:06:44 -0300

    comienzo funcionalidad grid productos

--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
+// Variables
+const gridRow = document.getElementById('gridRow')
+
 /*NAVABAR EFFECT */
 $(function () {
     $(document).scroll(function () {
@@ -8,3 +11,28 @@ $(function () {
 $(".img-fluid").addClass("wow fadeIn z-depth-1-half");
 
 
+function displayGridProducts(products) {
+
+  const almacenator = []
+  for (let i = 0; i < products.length; i++) {
+      const product = products[i];
+      const tbodyContent = `
+      <!--Grid column-->
+        <div class="col-lg-4 col-md-12 mb-4">
+  
+          <img src="${products.image}" class="img-fluid mb-4" alt="">
+  
+          <img src="${products.image}" class="img-fluid mb-4" alt=""
+            data-wow-delay="0.3s">
+  
+        </div>
+      `
+      almacenator.push(tbodyContent)
+  }
+  gridRow.innerHTML = almacenator.join('')
+}
+  function displayAllGridProducts() {
+    displayGridProducts(JSON.parse(localStorage.getItem('products')) || []);
+}
+
+displayAllGridProducts()
